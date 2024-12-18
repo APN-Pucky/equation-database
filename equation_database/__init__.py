db25fee9ba1eb48aa0385d2fce19207fc0586324
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2024-12-18 12:14:48 +0100

    Fix test 3

--- equation_database/__init__.py
+++ equation_database/__init__.py
@@ -36,7 +36,8 @@ def find(title=None, doi=None, isbn=None, eprint=None, key=None):
                 if entry.get("isbn"):
                     if entry["isbn"] == isbn:
                         return submodule
-                if entry.key() == key:
-                    return submodule
+                if entry.get("ID"):
+                    if entry["ID"] == key:
+                        return submodule
 
     return None
