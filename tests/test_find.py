7082325b871c0f455db731748ac4f3ffb32c27c8
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2024-12-18 11:40:10 +0100

    More find tests

--- tests/test_find.py
+++ tests/test_find.py
@@ -6,3 +6,24 @@ def test_find_arxiv():
     from equation_database import arxiv_2202_13416 as ipaper
 
     assert fpaper == ipaper
+
+
+def test_find_doi():
+    fpaper = edb.find(eprint="10.1103/PhysRev.176.1700")
+    from equation_database import doi_10_1103_physrev_176_1700 as ipaper
+
+    assert fpaper == ipaper
+
+
+def test_find_inspire():
+    fpaper = edb.find(eprint="Field:1989uq")
+    from equation_database import inspirehep_Field_1989uq as ipaper
+
+    assert fpaper == ipaper
+
+
+def test_find_isbn():
+    fpaper = edb.find(eprint="978-0-201-50397-5")
+    from equation_database import isbn_9780201503975 as ipaper
+
+    assert fpaper == ipaper
